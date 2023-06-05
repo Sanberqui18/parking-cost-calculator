e10052fe4e5c41b114c83611de2972bb1a61c21f
Santiago Bernal <san.be.qui18@gmail.com>
2023-06-05 18:26:33 -0500

    Add Cypress config and gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,18 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "cypress/e2e/**/*.js",
+    baseUrl: "https://www.shino.de/parkcalc/",
+    pageLoadTimeout: 5000,
+    defaultCommandTimeout: 10000,
+    screenshotOnRunFailure: true,
+    trashAssetsBeforeRuns: true,
+    video: true,
+    videoUploadOnPasses: false,
+    retries: {
+      openMode: 2,
+      runMode: 2
+    }
+
   },
 });
