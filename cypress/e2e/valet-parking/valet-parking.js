2b435bd6397a910d31f90c500a505430dc2a2bb4
Santiago Bernal <san.be.qui18@gmail.com>
2023-06-05 22:11:41 -0500

    Valet parking second test

--- cypress/e2e/valet-parking/valet-parking.js
+++ cypress/e2e/valet-parking/valet-parking.js
@@ -70,20 +70,19 @@ describe('Validate Valet Parking', () => {
     });
 
     it.only('Hours Calculation is correct', () => {
+        
         let startDate = "06/05/2023";
         let endDate = "06/05/2023";
+
         let startHour = "12:00"
         let startAMPM = "PM "
+
         let endHour = "3:30"
         let endAMPM = "PM "
 
+        let timeSpent;
         let expectedCost;
         
-        // if(timeSpent >= 24){
-        //     expectedCost = timeSpent/24 * 18
-        // }else{
-        //     expectedCost = 12;
-        // }
 
         cy.get("#ParkingLot").select("Valet Parking");
 
@@ -91,21 +90,30 @@ describe('Validate Valet Parking', () => {
         cy.get("#LeavingDate").clear().type(endDate);
 
         cy.get("#StartingTime").clear().type(startHour);
+        let startConverted = convert12To24(startHour, startAMPM);
+
         cy.get("#LeavingTime").clear().type(endHour);
+        let endConverted = convert12To24(endHour, endAMPM);    
 
         cy.get("input[name='StartingTimeAMPM'][type='radio']").eq(1).check();
         cy.get("input[name='LeavingTimeAMPM'][type='radio']").eq(1).check();
 
+        timeSpent = calculateTimeDifference(startDate, endDate, startConverted, endConverted);
 
+        if(timeSpent >= 24){
+            expectedCost = timeSpent/24 * 18
+        }else{
+            expectedCost = 12;
+        }
 
         cy.get("[type='submit']").click();
 
-        // cy.get(".SubHead > b").then((value) => {
-        //     let total = value.text().slice(2);
-        //     let cost = parseFloat(total);
-        //     cy.log(cost);
+        cy.get(".SubHead > b").then((value) => {
+            let total = value.text().slice(2);
+            let cost = parseFloat(total);
+            cy.log(cost);
         
-        //     expect(cost).to.eql(expectedCost);
-        // })
+            expect(cost).to.eql(expectedCost);
+        })
     });
 });
